6989f534611abd89e21f31f414030cf265386bb2
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-11-03 23:39:39 -0500

    Use finally to clean up after the fetch + formatting fixes

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
-//task 2: fetch tickets using async/await and handle errors
-
 const customerTickets = document.getElementById("customerTickets");
 const errorMessage = document.getElementById("errorMessage");
+const loading = document.getElementById("loading");
 
 async function fetchUnresolvedTickets() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    if (!response.ok) {
-        throw new Error("Failed to fetch customers.");
-    }
-    //error message if fetch fails
+    loading.style.display = "block";
+    //show loading indicator initially
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        if (!response.ok) {
+            throw new Error("Failed to fetch customers.");
+        }
+        //error message if fetch fails
 
-    const customer = await response.json();
-    console.log(response);
-    //function to fetch unresolved customer suport tickets
+        const customer = await response.json();
+        console.log(response);
+        //task 2 (above): fetch tickets using async/await and handle errors
 
-    customers.forEach(customer => {
-        const ticketItem = document.createElement('li');
-        ticketItem.textContent = `ID: ${data.id} - Name: ${data.userID} - Issue: ${data.title} - Details: ${data.body}`;
-        customerTickets.appendChild(listItem);
-    });
+        customer.forEach(ticket => {
+            const ticketItem = document.createElement('li');
+            ticketItem.innerHTML = 
+            `<p><b>ID:</b> ${ticket.id}</p>
+            <p><b>Name:</b> ${ticket.userId}</p>
+            <p><b>Issue:</b> ${ticket.title}</p>
+            <p><b>Details:</b> ${ticket.body}</p>
+            <br>`;
+            customerTickets.appendChild(ticketItem);
+        });
+        //formatting inside the html for organization
+        
+    } catch (error) {
+        errorMessage.textContent = error.message;
+        console.error(`Error fetching tickets: ${error}`);
     //task 3 (above): for each customer ticket, display details
-} 
+
+    } finally {
+        loading.style.display = "none"; //hide loading indicator even if error
+    }
+    //task 4 (above): use finally to ensure cleanup
+}
+
+fetchUnresolvedTickets();
+//call the function for anything to show up
