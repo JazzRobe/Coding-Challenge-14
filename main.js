fa8765f49d3e4ddaae9c384b9b1250d60d9d5ef0
Jazzmyne Roberson <jroberson0927@gmail.com>
2024-11-03 23:12:02 -0500

    Dynamically display tickets on the page

--- main.js
+++ main.js
@@ -7,7 +7,17 @@ async function fetchUnresolvedTickets() {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     if (!response.ok) {
         throw new Error("Failed to fetch customers.");
-    } //error message if fetch fails
+    }
+    //error message if fetch fails
+
     const customer = await response.json();
     console.log(response);
-} //function to fetch unresolved customer suport tickets
+    //function to fetch unresolved customer suport tickets
+
+    customers.forEach(customer => {
+        const ticketItem = document.createElement('li');
+        ticketItem.textContent = `ID: ${data.id} - Name: ${data.userID} - Issue: ${data.title} - Details: ${data.body}`;
+        customerTickets.appendChild(listItem);
+    });
+    //task 3 (above): for each customer ticket, display details
+} 
